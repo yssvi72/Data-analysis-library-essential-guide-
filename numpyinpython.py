7a56e209cd55d5466b8f0baede3fa38648563077
yssvi <141433982+yssvi72@users.noreply.github.com>
2024-06-12 10:20:43 +0530

    Update numpyinpython.py

--- numpyinpython.py
+++ numpyinpython.py
@@ -1,21 +1,8 @@
-# Importing the NumPy library with an alias 'np'
+#Numpy package to be installed
 import numpy as np
-
-# Printing the version of NumPy installed
-print(np.__version__)
-
-# Printing the configuration details of NumPy
-print(np.show_config())
-
-# Add the number :
-import numpy as np
-# Printing information about the np.add function using the np.info() method
-print(np.info(np.add))
 
 # Creating a NumPy array 'x' containing elements 1, 2, 3, and 4
-x = np.array([1, 2, 3, 4])
-
-# Printing the original array 'x'
+x = np.array([1, 2, 3, 4 , 5, 6])
 print("Original array:")
 print(x)
 
@@ -24,12 +11,14 @@ print("Test if none of the elements of the said array is zero:")
 print(np.all(x))
 
 # Reassigning array 'x' to a new array containing elements 0, 1, 2, and 3
-x = np.array([0, 1, 2, 3])
+x = np.array([0, 1, 2, 3,4,5,6])
 
-# Printing the new array 'x'
-print("Original array:")
+print(" Resizable  array:")
 print(x)
 
-# Checking if none of the elements in the updated array 'x' is zero and printing the result
-print("Test if none of the elements of the said array is zero:")
-print(np.all(x))
+
+
+
+
+
+
